d139def9c205315034b4466cdb0c0ff9d57258f0
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-10 02:29:48 +0600

    teaching animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     './src/**/*.{html,js}', // Scan all .html and .js files inside src and its subfolders
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'wave-text': 'waveText 3s linear infinite',
+      },
+      keyframes: {
+        waveText: {
+          '0%': { 'background-position': '0 0' },
+          '100%': { 'background-position': '100% 0' },
+        },
+      },
+    },
   },
   plugins: [
     require('daisyui'),
